e89564bd2f1fa53b91fc89b52a0adc8463e18e4c
jadebugnos <173978908+jadebugnos@users.noreply.github.com>
2024-11-04 07:36:31 +0800

    Add operate function for basic arithmetic
    -Declare operandOne, operandTwo, and operator
    variables for initial setup

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function divide(...num) {
     return num.reduce((a, b) => a / b)
 }
 
+let operandOne = 23;
+let operandtwo = 34;
+let operator = '/';
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+console.log(operate(operandOne, operandtwo, operator))
