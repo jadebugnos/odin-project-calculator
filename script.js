0714f8994743effebb39b4b674cd1ae5a51b2b69
jadebugnos <173978908+jadebugnos@users.noreply.github.com>
2024-11-04 20:46:52 +0800

    enhance populate function for better functionality
    -add event listener to operator buttons to store
    input to an array

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function divide(...num) {
     return num.reduce((a, b) => a / b)
 }
 
-let operandOne = 23;
-let operandtwo = 34;
-let operator = '/';
+let firstOperand = [];
+let secondOperand = [];
+let operator;
 
 function operate(num1, num2, operator) {
     switch (operator) {
@@ -32,13 +32,41 @@ function operate(num1, num2, operator) {
     }
 }
 
+
 function populate() {
     const display = document.getElementById('display');
     const buttonsContainer = document.querySelector('#button-containers');
+    let acc = '';
 
     buttonsContainer.addEventListener('click', (e) => {
         if (e.target !== e.currentTarget && e.target.classList.contains('digit')) {
             display.textContent += e.target.value;
+            acc += e.target.value;
+        }
+
+        if (e.target !== e.currentTarget && e.target.classList.contains('operator')) {
+            switch (e.target.id) {
+                case 'plus':
+                    operator = '+'
+                    firstOperand.push(Number(acc))
+                    console.log(firstOperand, operator)
+                    break;
+                case 'minus':
+                    operator = '-';
+                    firstOperand.push(Number(acc))
+                    console.log(firstOperand, operator)
+                    break;
+                case 'times':
+                    operator = '*';
+                    firstOperand.push(Number(acc))
+                    console.log(firstOperand, operator)
+                    break;
+                case 'divide':
+                    operator = '/';
+                    firstOperand.push(Number(acc))
+                    console.log(firstOperand, operator)
+                    break;
+            }
         }
     })
 }
