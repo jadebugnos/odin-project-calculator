4858781a324f5dd09d4ef2569dc4eff07418be2c
jadebugnos <173978908+jadebugnos@users.noreply.github.com>
2024-11-05 09:04:35 +0800

    enhance calculator logic by adding active switch
    -clear display when operator button is press
    -refactor long winded code and add handleDisplay
    function

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function divide(...num) {
     return num.reduce((a, b) => a / b)
 }
 
+
 let firstOperand = [];
 let secondOperand = [];
 let operator;
@@ -33,22 +34,35 @@ function operate(num1, num2, operator) {
 }
 
 
+let active = false;
+
 function populate() {
     const display = document.getElementById('display');
     const buttonsContainer = document.querySelector('#button-containers');
     let acc = '';
 
     buttonsContainer.addEventListener('click', (e) => {
-        //this is for the numbers buttons when clicked it will update ui display
-        if (e.target !== e.currentTarget && e.target.classList.contains('digit')) {
-            display.textContent += e.target.value;
-            acc += e.target.value;
-        }
-        //this is for operator buttons when pushed it will update operator then push the number to the array
-        if (e.target !== e.currentTarget && e.target.classList.contains('operator')) {
-            handleOperatorClick(e.target.id, acc)
+
+        if (e.target !== e.currentTarget) {
+            if (e.target.classList.contains('digit')) {
+                if (!active) {
+                    handleDisplay(e.target.value, display)
+                    acc += e.target.value;
+                } else {
+                    handleDisplay(e.target.value, display)
+                }
+            } else if (e.target.classList.contains('operator')) {
+                handleOperatorClick(e.target.id, acc)
+            }
         }
     })
+}
+
+function handleDisplay(value, display) {
+    active ? display.textContent = value :
+        display.textContent += value
+        active = false
+        console.log(active)
 }
 
 
@@ -65,6 +79,8 @@ function handleOperatorClick(targetId, acc) {
     if (operator) {
         firstOperand.push(converted)
         console.log(firstOperand, operator)
+        active = true;
+        console.log(active)
     }
 }
 
